5d6236a3c5ad35ecf948c27ad9845adba8da0ce2
Peter Taoussanis <ptaoussanis@gmail.com>
2012-12-09 20:16:48 +0700

    Update bench results

--- src/servers.clj
+++ src/servers.clj
@@ -44,7 +44,8 @@
 ;;;; Results post-warmup OpenJDK7 -server, 1.7GHz i5
 ;;;; ab -n 5000 -c4 http://localhost:[port]/
 ;; nginx 1.2.5  ; ~10,000 reqs/sec
+;; :aloha       ;  ~7,800 reqs/sec
 ;; :jetty       ;  ~6,100 reqs/sec
 ;; :simple      ;  ~4,600 reqs/sec
 ;; :aleph-sync  ;  ~2,800 reqs/sec
-;; :aleph-async ;  ~4,200 reqs/seq
+;; :aleph-async ;  ~4,500 reqs/sec

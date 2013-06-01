2cb6b8373ee2c7383632e537fee2f177fb0c1969
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-06-02 03:39:41 +0530

    add Jetty 9 server test

---
+++ servers/jetty9/project.clj
@@ -0,0 +1,15 @@
+(defproject jetty9 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :warn-on-reflection true
+  :plugins [[lein-servlet "0.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :aot [jetty9.servlet]
+  :jvm-opts ["-server" "-XX:+UseConcMarkSweepGC"]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :servlet {:deps    [[lein-servlet/adapter-jetty9  "0.3.0"]]
+            :config  {:port 8093}
+            :webapps {"/" {:servlets {"/*" 'jetty9.servlet}
+                           :public "."}}})

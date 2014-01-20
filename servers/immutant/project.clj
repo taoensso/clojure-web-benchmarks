c9ffc5cdae8299b00f03005a02ca916bb84346f8
Jim Crossley <jim@crossleys.org>
2014-01-20 12:08:59 -0500

    Include latest release of Immutant in tests

---
+++ servers/immutant/project.clj
@@ -0,0 +1,8 @@
+(defproject immutant "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-immutant "1.1.1"]]
+  :profiles {:benchmark {:immutant {:context-path "/", :nrepl-port nil}}})

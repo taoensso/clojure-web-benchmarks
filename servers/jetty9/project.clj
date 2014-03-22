dca68eea416ef112184588d5a83525680c9dfe5c
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-03-22 14:27:23 +0530

    use lein-servlet 0.4.0 for updated servlet adapter versions

--- servers/jetty9/project.clj
+++ servers/jetty9/project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :warn-on-reflection true
-  :plugins [[lein-servlet "0.3.0"]]
+  :plugins [[lein-servlet "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :aot [jetty9.servlet]
   :jvm-opts ["-server" "-XX:+UseConcMarkSweepGC"]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :servlet {:deps    [[lein-servlet/adapter-jetty9  "0.3.0"]]
+  :servlet {:deps    [[lein-servlet/adapter-jetty9  "0.4.0"]]
             :config  {:port 8093}
             :webapps {"/" {:servlets {"/*" 'jetty9.servlet}
                            :public "."}}})

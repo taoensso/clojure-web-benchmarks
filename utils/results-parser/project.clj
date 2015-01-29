b5fa03bf208c10dccf945a25be4257576f5fd9df
xfeep <xfeepz@gmail.com>
2015-01-30 00:44:39 +0800

    add wrk2 results parser for generating text table & charts

--- utils/results-parser/project.clj
+++ utils/results-parser/project.clj
@@ -1,4 +1,4 @@
-(defproject nginx-clojure "0.1.0-SNAPSHOT"
+(defproject results-parser "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,10 @@
 ;  :plugins [[lein-shell "0.4.0"]]
   :dependencies 
   [[org.clojure/clojure    "1.7.0-alpha3"]
+    [table "0.4.0"] 
+    [incanter "1.5.6"]
     ]
   :jvm-opts ["-server" "-XX:+UseConcMarkSweepGC"]
   :profiles {:1.7.0-alpha3 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]}}
-  :main nginx-clojure
+  :main wrk2-result-parser
 )

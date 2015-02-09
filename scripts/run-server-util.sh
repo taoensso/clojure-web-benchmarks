e58e3ae3bee64b0e30128077562fb4e2e4da146b
xfeep <xfeepz@gmail.com>
2015-02-09 17:50:55 +0700

    @xfeep/wrk2-merge: add high connection count tests

---
+++ scripts/run-server-util.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+start_server() {
+    cd servers/$1
+    echo "Starting server in $(pwd)..."
+    (nohup lein with-profile 1.5 trampoline run 1>>../../logs/run-servers 2>&1 &)
+    cd ../../
+}
+
+start_servlet() {
+    cd servers/$1
+    echo "Starting servlet in $(pwd)..."
+    (nohup lein with-profile 1.5 trampoline servlet  run 1>>../../logs/run-servers 2>&1 &)
+    cd ../../
+}
+
+start_nginx_clojure() {
+    cd servers/nginx-clojure
+    echo "Prepare nginx-clojure in $(pwd)..."
+    (nohup lein with-profile 1.7.0-alpha3 trampoline  run $1 1>>../../logs/run-servers 2>&1)
+    echo "Starting nginx-clojure in $(pwd)..."
+    ./nginx
+    cd ../..
+}
+
+start_immutant() {
+    cd servers/immutant
+    export LEIN_IMMUTANT_BASE_DIR=.install
+    echo "Starting Immutant in $(pwd)..."
+    if [[ "$IMMUTANT" == "servlet" ]]; then
+        (nohup lein with-profiles benchmark,servlet do compile, immutant server 8095 1>>../../logs/run-servers 2>&1 &)
+    else
+        (nohup lein with-profile benchmark immutant server 8095 1>>../../logs/run-servers 2>&1 &)
+    fi
+    cd ../..
+}
--- scripts/run-servers-for-largeNumofConns.sh
+++ scripts/run-servers-for-largeNumofConns.sh
@@ -4,6 +4,8 @@ source scripts/run-server-util.sh
 
 mkdir -p logs
 
+
+
 start_server  "embedded"
 start_servlet "tomcat7"
 start_servlet "tomcat8"
@@ -12,10 +14,11 @@ start_servlet "jetty8"
 start_servlet "jetty9"
 
 
-start_nginx_clojure "1400"
+start_nginx_clojure "65000"
 echo "If you cannot start nginx-clojure, please check jvm configuration in the file ../servers/nginx-clojure/conf/nginx.conf"
 
-start_immutant
+## immutant 1 has too many error under large number of connections so we just remove it now
+#start_immutant
 
 tail -fn 0 logs/run-servers
 
